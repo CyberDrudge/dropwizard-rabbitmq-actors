f014c50b33341813540d84a91f8d6bf48e808e9c
Satyam Saxena <satyam.saxena@phonepe.com>
2024-01-03 15:41:18 +0530

    Update ObserverContext annotations

--- src/main/java/io/appform/dropwizard/actors/observers/ObserverContext.java
+++ src/main/java/io/appform/dropwizard/actors/observers/ObserverContext.java
@@ -2,14 +2,12 @@ package io.appform.dropwizard.actors.observers;
 
 import io.appform.dropwizard.actors.common.RMQOperation;
 import lombok.Builder;
-import lombok.Data;
-import lombok.Setter;
+import lombok.Value;
 
 import java.util.Map;
 
-@Data
+@Value
 @Builder
-@Setter
 public class ObserverContext {
     RMQOperation operation;
     String queueName;
